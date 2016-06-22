c933e384f6f795ad4b65130a79ae6d829597a698
Andrea Gangl <e1025756@student.tuwien.ac.at>
2016-06-23 01:24:33 +0200

    small update

--- Task2/train.m
+++ Task2/train.m
@@ -6,7 +6,6 @@ img = zeros(size(images,1),size(images,2));
 n = nnz(masks); % number of non zero elements in mask
 img(masks==10) = images(masks==10);
 
-
 features = computeFeatures(img);
     
 result = TreeBagger(32,features',masks,'OOBVarImp','on');
